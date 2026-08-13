75a05734af13086777195f537478bea165f68b83
Aditya <adityabasanti@gmail.com>
2026-08-14 00:04:06 +0530

    fix:fixed the footer date

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import "./Footer.css";
 export const Footer = () => {
   return (
     <footer>
-      <span className="footer">2025 @Portfolio.me</span>
+      <span className="footer">{new Date().getFullYear()} @Portfolio.me</span>
     </footer>
   );
 };
